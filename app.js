6ec3ea56715f5f2ba8f900318c7f939c355b4792
Liviu Buciuman <34024562+LiviuBuciuman@users.noreply.github.com>
2020-01-06 13:09:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const s = "07:05:45PM";
+const s = "12:05:39AM";
 console.log(timeConversion(s));
 
 function timeConversion(s) {
@@ -6,7 +6,19 @@ function timeConversion(s) {
     const hour = s.substring(0, 2);
     const min = s.substr(3, 2);
     const sec = s.substr(6, 2);
-    let hourIn24 = 0;
-    hourIn24 = (meridian.toUpperCase() === "PM") ? parseInt(hour) + 12 : parseInt(hour);
+    let hourIn24 = "00";
+    if (meridian.toUpperCase() === "PM") {
+        if (hour === "12") {
+            hourIn24 = "12";
+        } else {
+            hourIn24 = parseInt(hour) + 12;
+        }
+    } else {
+        if (hour === "12") {
+            hourIn24 = "00";
+        } else {
+            hourIn24 = hour;
+        }
+    }
     return hourIn24.toString() + ":" + min + ":" + sec;
 }
